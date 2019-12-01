0e01bf025cd969bd1abacfee75932a6616a08b9f
Sam Adams <samharad@umich.edu>
2019-12-01 01:12:59 -0500

    day 1

--- src/days/one.clj
+++ src/days/one.clj
@@ -1,2 +1,35 @@
-(ns days.one)
+(ns days.one
+  (:require [clojure.string :refer [split-lines]]))
 
+(defn read-input-lines [f-name]
+  (split-lines (slurp f-name)))
+
+(defn fuel-for-mass
+  "Does NOT account for the mass of the fuel itself."
+  [mass]
+  (-> mass (/ 3) (long) (- 2)))
+
+(defn adjust-fuel-mass
+  "Adjusts a 'raw' fuel mass to account for
+  the mass of the fuel itself."
+  [fuel-mass]
+  (if (pos? fuel-mass)
+    (+ fuel-mass (adjust-fuel-mass
+                   (fuel-for-mass fuel-mass)))
+    0))
+
+(defn fuel-for-mass-1
+  "Accounts for the mass of the fuel itself."
+  [mass]
+  (adjust-fuel-mass (fuel-for-mass mass)))
+
+(defn- solution [f-name f]
+  (let [lines (read-input-lines f-name)]
+    (apply + (map (comp f #(Long/valueOf %))
+                  lines))))
+
+(defn solution-a [f-name]
+  (solution f-name fuel-for-mass))
+
+(defn solution-b [f-name]
+  (solution f-name fuel-for-mass-1))
